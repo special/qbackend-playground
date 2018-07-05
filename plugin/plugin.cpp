94281263b7655fdae80e5716bd6a0fa6c2a9f833
John Brooks <john.brooks@dereferenced.net>
2018-07-05 13:29:37 -0700

    Expose BackendConnection as a singleton

--- plugin/plugin.cpp
+++ plugin/plugin.cpp
@@ -9,6 +9,14 @@
 void QBackendPlugin::registerTypes(const char *uri)
 {
     qmlRegisterType<QBackendConnection>(uri, 1, 0, "BackendConnection");
+    qmlRegisterSingletonType<QBackendConnection>(uri, 1, 0, "Backend",
+        [](QQmlEngine *engine, QJSEngine *scriptEngine) -> QObject*
+        {
+            Q_UNUSED(engine);
+            Q_UNUSED(scriptEngine);
+            return new QBackendConnection;
+        }
+    );
     qmlRegisterType<QBackendProcess>(uri, 1, 0, "BackendProcess");
     qRegisterMetaType<QBackendObject*>();
     qRegisterMetaType<QBackendModel*>();
